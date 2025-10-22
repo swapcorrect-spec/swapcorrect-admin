0231290d19f39553fcdc21e9409f1d9158e869a0
Nelson-Chinedu <egwuchinedu69@gmail.com>
2025-10-22 10:51:00 +0100

    chore: setup api config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Routes from "./modules/routes";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+    mutations: {},
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Routes />
       <Outlet />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
